f31ef88c1773f2fdc81603e9c92ddfb7260481a6
Greyti <47736158+Greyti8888@users.noreply.github.com>
2019-08-13 15:10:36 +0300

    Updates

---
+++ precache-manifest.89d1e725b66a6ad972f119d74aad3aa1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119dddd32fea4d774d5a3458aa434447",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "be288e002ffb45d68c47",
+    "url": "/pomodoro-clock/static/css/2.8daf6b99.chunk.css"
+  },
+  {
+    "revision": "be288e002ffb45d68c47",
+    "url": "/pomodoro-clock/static/js/2.60f8da3e.chunk.js"
+  },
+  {
+    "revision": "6d6b7d94792281e92c1a",
+    "url": "/pomodoro-clock/static/js/main.68083717.chunk.js"
+  },
+  {
+    "revision": "a94c9c237e61dbb35ae8",
+    "url": "/pomodoro-clock/static/js/runtime~main.b8c7466b.js"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/pomodoro-clock/static/media/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/pomodoro-clock/static/media/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "f249e44df3044e7b0d665b550569ddf0",
+    "url": "/pomodoro-clock/static/media/fa-brands-400.f249e44d.svg"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/pomodoro-clock/static/media/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/pomodoro-clock/static/media/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/pomodoro-clock/static/media/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "2fad4ee0a47f41bc76e21c2baf613182",
+    "url": "/pomodoro-clock/static/media/fa-regular-400.2fad4ee0.svg"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/pomodoro-clock/static/media/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/pomodoro-clock/static/media/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/pomodoro-clock/static/media/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/pomodoro-clock/static/media/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/pomodoro-clock/static/media/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/pomodoro-clock/static/media/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/pomodoro-clock/static/media/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "fdc155d57b7351ef85b3028ea3cfc048",
+    "url": "/pomodoro-clock/static/media/fa-solid-900.fdc155d5.svg"
+  }
+]);
